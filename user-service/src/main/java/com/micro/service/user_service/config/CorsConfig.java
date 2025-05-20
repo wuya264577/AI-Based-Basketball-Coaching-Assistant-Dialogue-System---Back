a77f0948c9da37dcb76c8f358c45a33668c728f1
2252635 <152025278+2252635@users.noreply.github.com>
2025-05-20 14:59:22 +0800

    补充

---
+++ user-service/src/main/java/com/micro/service/user_service/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.micro.service.user_service.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:8080"); // 前端地址
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true); // 如果前端携带 Cookie 或 Authorization 头
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
